26fadc6011967dbf4d66029d18140264e13796cf
Valentin Chaillou <vchaillo@student.42.fr>
2023-04-27 01:18:12 +0200

    clean some garbage stuff

--- expressions.js
+++ expressions.js
@@ -72,6 +72,25 @@ fetch('https://api.notion.com/v1/databases/48e67affd6bb45568f9358efd0abd727/quer
     console.error(error);
 })
 
+fetch('https://swapi.dev/api/people', {
+    method: 'GET',
+    // headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer secret_6uURx5tlKpgue1jmTpC8JKJN9JW4d9OiAZnQhcKm67B',
+    //     'Notion-Version': '2022-06-28',
+    // },
+    // mode: 'no-cors',
+    // body: JSON.stringify({ "id": 78912 })
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
+.catch(error => {
+    console.error(error);
+})
+
+
+
 /////////////////////////////////////////////////////////////////////
 //                      UPDATE EXPRESSION                          //
 /////////////////////////////////////////////////////////////////////
